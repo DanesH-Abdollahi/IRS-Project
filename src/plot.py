b09fea52766b9f84614d47962f80e138514159e0
DanesH-Abdollahi <danesh.abdollahii@gmail.com>
2023-06-19 19:50:43 +0330

    Modifications

--- src/plot.py
+++ src/plot.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
 
-def plot(*, score_history, sumrate, u1_sinr, u2_sinr=None, mean: bool = False, title: str = "Title"):
+def plot(*, score_history, sumrate, u1_sinr, u2_sinr=None, mean: bool = False, title: str = "Title", method: str):
+    ts = datetime.datetime.now().strftime("%Y%m%d_%H.%M.%S")
     if mean:
         sumrate = np.mean(sumrate, axis=0)
         u1_sinr = np.mean(u1_sinr, axis=0)
@@ -14,16 +16,18 @@ def plot(*, score_history, sumrate, u1_sinr, u2_sinr=None, mean: bool = False, t
         if u2_sinr is not None:
             u2_sinr = u2_sinr.reshape((u2_sinr.shape[0] * u2_sinr.shape[1], ))
 
-    # Plot the score history
-    plt.plot(range(1, len(score_history) + 1),
-             score_history, linewidth=1.5, label='Score')
-    plt.title(title, fontweight='bold')
-    plt.ylabel('Score')
-    plt.xlabel('Episode')
-    plt.grid(1)
-    # plt.savefig('../tmp_results/Score.png')
-    plt.savefig('../../Score.png', bbox_inches='tight')
-    plt.show()
+    if method != "Iterations_Only":
+        # Plot the score history
+        plt.plot(range(1, len(score_history) + 1),
+                 score_history, linewidth=1.5, label='Score')
+        plt.title(title, fontweight='bold')
+        plt.ylabel('Score')
+        plt.xlabel('Episode')
+        plt.grid(1)
+        # plt.savefig('../tmp_results/Score.png')
+        plt.savefig(f'../Results/{method}/Score_{ts}.svg',
+                    bbox_inches='tight')
+        plt.show()
 
     # Plot the sumrate
     plt.plot(range(1, len(sumrate) + 1), sumrate,
@@ -47,10 +51,11 @@ def plot(*, score_history, sumrate, u1_sinr, u2_sinr=None, mean: bool = False, t
                 color='k', label=f'Max = {sumrate.max() : < .3}')
     plt.legend(bbox_to_anchor=(1.0, 1), loc='best')
     # plt.savefig('../tmp_results/Sumrate.png')
-    plt.savefig('../../Sumrate.png', bbox_inches='tight')
+    plt.savefig(f'../Results/{method}/Sumrate_{ts}.svg',
+                bbox_inches='tight')
     plt.show()
 
-    # Plot the U1_SINR
+    # Plot the U1_SINR in normal scale
     plt.plot(range(1, len(u1_sinr) + 1), u1_sinr,
              linewidth=1.5, label='U1 SINR')
     # plt.yscale('log')
@@ -75,11 +80,38 @@ def plot(*, score_history, sumrate, u1_sinr, u2_sinr=None, mean: bool = False, t
                 label=f'Max = {u1_sinr.max() : < .3}')
     plt.legend(bbox_to_anchor=(1.0, 1), loc='best')
     # plt.savefig('../tmp_results/U1_SINR.png')
-    plt.savefig('../../U1_SINR.png', bbox_inches='tight')
+    plt.savefig(f'../Results/{method}/U1_SINR_{ts}.svg', bbox_inches='tight')
+    plt.show()
+
+    # Plot the U1_SINR in dB
+    plt.plot(range(1, len(u1_sinr) + 1), 10 *
+             np.log10(u1_sinr), linewidth=1.5, label='U1 SINR')
+    plt.title(title, fontweight='bold')
+    plt.ylabel('U1 SINR (dB)')
+    plt.xlabel('Iteration')
+    plt.grid(1)
+    moving_average = np.zeros((len(u1_sinr)))
+    window_size = 100
+    for i in range(len(u1_sinr)):
+        if i < window_size:
+            moving_average[i] = np.mean(u1_sinr[:i + 1])
+        else:
+            moving_average[i] = np.mean(u1_sinr[i - window_size + 1:i + 1])
+
+    plt.plot(range(1, len(moving_average) + 1), 10 * np.log10(moving_average),
+             linewidth=1.3, label=f'MA 100 , {10 * np.log10(moving_average[-1]) : < .3}')
+
+    # plt.axhline(y=10 * np.log10(u1_sinr.mean()), xmin=0, xmax=1, color='r', label='Mean')
+    plt.axhline(y=10 * np.log10(u1_sinr.max()), xmin=0, xmax=1, color='k',
+                label=f'Max = {10 * np.log10(u1_sinr.max()) : < .3}')
+    plt.legend(bbox_to_anchor=(1.0, 1), loc='best')
+    # plt.savefig('../tmp_results/U1_SINR_dB.png')
+    plt.savefig(
+        f'../Results/{method}/U1_SINR_dB_{ts}.svg', bbox_inches='tight')
     plt.show()
 
     if u2_sinr is not None:
-        # Plot the U2_SINR
+        # Plot the U2_SINR in normal scale
         plt.plot(range(1, len(u2_sinr) + 1), u2_sinr,
                  linewidth=1.5, label='U2 SINR')
         # plt.yscale('log')
@@ -103,5 +135,33 @@ def plot(*, score_history, sumrate, u1_sinr, u2_sinr=None, mean: bool = False, t
 
         plt.legend(bbox_to_anchor=(1.0, 1), loc='best')
         # plt.savefig('../tmp_results/U2_SINR.png')
-        plt.savefig('../../U2_SINR.png', bbox_inches='tight')
+        plt.savefig(
+            f'../Results/{method}/U2_SINR_{ts}.svg', bbox_inches='tight')
+        plt.show()
+
+        # Plot the U2_SINR in dB
+        plt.plot(range(1, len(u2_sinr) + 1), 10 *
+                 np.log10(u2_sinr), linewidth=1.5, label='U2 SINR')
+        plt.title(title, fontweight='bold')
+        plt.ylabel('U2 SINR (dB)')
+        plt.xlabel('Iteration')
+        plt.grid(1)
+        moving_average = np.zeros((len(u2_sinr)))
+        window_size = 100
+        for i in range(len(u2_sinr)):
+            if i < window_size:
+                moving_average[i] = np.mean(u2_sinr[:i + 1])
+            else:
+                moving_average[i] = np.mean(u2_sinr[i - window_size + 1:i + 1])
+
+        plt.plot(range(1, len(moving_average) + 1), 10 * np.log10(moving_average),
+                 linewidth=1.3, label=f'MA 100 , {10 * np.log10(moving_average[-1]) : < .3}')
+
+        # plt.axhline(y=10 * np.log10(u2_sinr.mean()), xmin=0, xmax=1, color='r', label='Mean')
+        plt.axhline(y=10 * np.log10(u2_sinr.max()), xmin=0, xmax=1, color='k',
+                    label=f'Max = {10 * np.log10(u2_sinr.max()) : < .3}')
+        plt.legend(bbox_to_anchor=(1.0, 1), loc='best')
+        # plt.savefig('../tmp_results/U2_SINR_dB.png')
+        plt.savefig(
+            f'../Results/{method}/U2_SINR_dB_{ts}.svg', bbox_inches='tight')
         plt.show()

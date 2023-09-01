6a616c4fcc134b52f65b64ed541ac297514b9d1b
DanesH-Abdollahi <danesh.abdollahii@gmail.com>
2023-09-01 16:40:30 +0330

    Modification

--- src/Networks.py
+++ src/Networks.py
@@ -17,30 +17,32 @@ class CriticNetwork(keras.Model):
             self.chkpt_dir, self.model_name + "_ddpg.h5")
 
         self.bn0 = BatchNormalization()
-        self.fc1 = Dense(self.fc1_dims, activation='relu')
+        self.fc1 = Dense(1024, activation='relu')
+        self.fc2 = Dense(512, activation='relu')
         self.bn1 = BatchNormalization()
-        # self.fc2 = Dense(self.fc2_dims, activation='relu')
-        self.bn2 = BatchNormalization()
 
-        self.action_value = Dense(self.fc1_dims, activation='relu')
+        # self.action_value_1 = Dense(1024, activation='relu')
+        self.action_value_2 = Dense(256, activation='relu')
+        self.action_value_3 = Dense(128, activation='relu')
+        self.action_value_4 = Dense(64, activation='relu')
         self.concat = tf.keras.layers.Concatenate()
         # self.output_layer1 = Dense(self.fc1_dims, activation="relu")
-        self.output_layer2 = Dense(self.fc2_dims, activation="relu")
+        # self.output_layer2 = Dense(self.fc2_dims, activation="relu")
         self.q = Dense(1, activation=None)
 
     def call(self, state, action):
-        state_value = self.bn0(state)
+        # combined = self.concat([state, action])
+        state_value = self.bn0(action)
         state_value = self.fc1(state_value)
-        # state_value = self.fc2(state_value)
-        # state_value = self.bn2(state_value)
+        state_value = self.fc2(state_value)
+        combined = self.concat([state_value, state])
+        combined = self.bn1(combined)
+        # action_value = self.action_value_1(combined)
+        action_value = self.action_value_2(combined)
+        action_value = self.action_value_3(action_value)
+        action_value = self.action_value_4(action_value)
+        q = self.q(action_value)
 
-        action_value = self.bn1(action)
-        action_value = self.action_value(action_value)
-        q = self.concat([state_value, action_value])
-        q = self.bn2(q)
-        # q = self.output_layer1(q)
-        q = self.output_layer2(q)
-        q = self.q(q)
         return q
 
 
@@ -58,32 +60,25 @@ class ActorNetwork(keras.Model):
             self.chkpt_dir, self.model_name + "_ddpg.h5")
 
         self.bn0 = BatchNormalization()
-        self.fc1 = Dense(self.fc1_dims, activation='relu')
-        # self.fc2 = Dense(self.fc1_dims, activation='relu')
-        self.fc3 = Dense(self.fc2_dims, activation='relu')
+        # self.fc01 = Dense(3010, activation='relu')
+        self.fc0 = Dense(2024, activation='relu')
+        self.fc1 = Dense(1024, activation='relu')
+        self.fc2 = Dense(512, activation='relu')
+        self.fc3 = Dense(256, activation='relu')
         self.fc4 = Dense(64, activation='relu')
+        # self.fc4 = Dense(64, activation='relu')
         self.mu = Dense(self.n_actions - 1, activation='sigmoid')
-        # self.concat = tf.keras.layers.Concatenate()
-        # self.power1 = Dense(128, activation="sigmoid")
-        # self.power2 = Dense(64, activation="sigmoid")
-        # self.power3 = Dense(32, activation="sigmoid")
-        # self.power4 = Dense(1, activation="sigmoid")
 
     def call(self, state):
         prob = self.bn0(state)
-        # power = self.power1(prob)
-
+        # prob = self.fc01(prob)
+        prob = self.fc0(prob)
         prob = self.fc1(prob)
-        # prob = self.fc2(prob)
+        prob = self.fc2(prob)
         prob = self.fc3(prob)
         prob = self.fc4(prob)
-
         mu = self.mu(prob) * self.bound
-        # power = self.power2(power)
-        # power = self.power3(power)
-        # power = self.power4(power)
 
-        # out = self.concat([mu, power])
         return mu
 
 
@@ -98,17 +93,11 @@ class PowerActorNetwork(keras.Model):
             self.chkpt_dir, self.model_name + "_ddpg.h5")
 
         self.bn0 = BatchNormalization()
-        # self.fc1 = Dense(1024, activation='relu')
-        # self.fc2 = Dense(512, activation='relu')
-        self.fc3 = Dense(128, activation='relu')
-        # self.fc4 = Dense(64, activation='relu')
-        self.fc5 = Dense(num_of_users - 1, activation='sigmoid')
+        self.fc1 = Dense(128, activation='relu')
+        self.fc2 = Dense(num_of_users - 1, activation='sigmoid')
 
     def call(self, state):
         power = self.bn0(state)
-        # power = self.fc1(power)
-        # power = self.fc2(power)
-        power = self.fc3(power)
-        # power = self.fc4(power)
-        power = self.fc5(power)
+        power = self.fc1(power)
+        power = self.fc2(power)
         return power
